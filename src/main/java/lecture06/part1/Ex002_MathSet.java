96f37e2e814576d7e9c54ccc435edd27d0d7b014
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2023-03-19 22:34:26 +1000

    added lecture06

---
+++ src/main/java/lecture06/part1/Ex002_MathSet.java
@@ -0,0 +1,24 @@
+package lecture06.part1;
+import java.util.*;
+public class Ex002_MathSet {
+    public static void main(String[] args) {
+        var a = new HashSet<>(Arrays.asList(1,2,3,4,5,6,7));
+        var b = new HashSet<>(Arrays.asList(2,3,5,7,11,13,17));
+        var u = new HashSet<Integer>(a);
+//        Объединение
+        u.addAll(b);
+        var r = new HashSet<Integer>(a);
+//        Пересечение
+        r.retainAll(b);
+        var s = new HashSet<Integer>(a);
+//        Разность
+        s.removeAll(b);
+        System.out.println(a); // [1, 2, 3, 4, 5, 6]
+        System.out.println(b); // [17, 2, 3, 5, 7, 11]
+        System.out.println(u); // [1, 17, 2, 3, 4, 5, 6, 7, 11, 13]
+        System.out.println(r); // [2, 3, 5, 7]
+        System.out.println(s); // [1, 4, 6]
+
+        System.out.println(a.addAll(b));
+    }
+}
